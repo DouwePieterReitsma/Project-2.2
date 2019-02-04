eacb042dc57116345cad9e508c804fdb1f53063e
DouwePieterReitsma <d.p.reitsma@gmail.com>
2019-02-04 16:44:24 +0100

    login.php added

---
+++ frontend/login.php
@@ -0,0 +1,13 @@
+<?php
+
+session_start();
+
+$username = array_key_exists('username', $_POST) ? trim($_POST['username']) : null;
+$password = array_key_exists('password', $_POST) ? trim($_POST['password']) : null;
+
+if(empty($username) || empty($password))
+{
+    $_SESSION['message'] = "Invalid credentials.";
+    exit;
+}
+
